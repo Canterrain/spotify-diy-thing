c8a33bee8262fb77cfb84306d490cb64470af801
Josh <canterrain@live.com>
2024-08-26 10:21:00 -0400

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,42 +1,45 @@
-// import { NowPlayingContextProvider } from "~/components/context/NowPlayingContext";
+import { NowPlayingContextProvider } from "~/components/context/NowPlayingContext";
 import SpotifyNowPlaying from "~/components/SpotifyNowPlaying";
-import {
-  getAuthTokenSignal,
-  useSpotifyAuth,
-} from "~/components/hooks/useSpotifyAuth";
-import { trpc } from "~/utils/trpc";
+import { getAuthTokenSignal, useSpotifyAuth } from "~/components/hooks/useSpotifyAuth";
+import { onMount } from "solid-js"; // Import onMount for client-side specific actions
 
 const CARD_SIZE = 715;
 
 export default function Home() {
   useSpotifyAuth();
 
+  onMount(() => {
+    // Code inside onMount only runs in the browser
+  });
+
   return (
     <main>
-      {getAuthTokenSignal().length > 0 && (
-        <div class="flex items-center justify-center w-screen h-screen text-white">
-          <div
-            class="relative flex"
-            style={{
-              flex: "none",
-              width: `${CARD_SIZE}px`,
-              height: `${CARD_SIZE}px`,
-            }}
-          >
+      {typeof window !== "undefined" && getAuthTokenSignal().length > 0 && (
+        <NowPlayingContextProvider>
+          <div class="flex items-center justify-center w-screen h-screen text-white">
             <div
-              class="relative text-white rounded-3xl overflow-hidden"
+              class="relative flex"
               style={{
                 flex: "none",
                 width: `${CARD_SIZE}px`,
                 height: `${CARD_SIZE}px`,
-                transform: `translateZ(0)`, // Fix for rounded corners in webkit
-                "scroll-snap-align": "center",
               }}
             >
-              <SpotifyNowPlaying />
+              <div
+                class="relative text-white rounded-3xl overflow-hidden"
+                style={{
+                  flex: "none",
+                  width: `${CARD_SIZE}px`,
+                  height: `${CARD_SIZE}px`,
+                  transform: `translateZ(0)`, // Fix for rounded corners in webkit
+                  "scroll-snap-align": "center",
+                }}
+              >
+                <SpotifyNowPlaying />
+              </div>
             </div>
           </div>
-        </div>
+        </NowPlayingContextProvider>
       )}
     </main>
   );
